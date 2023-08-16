7b717278bb2fa78bf907c06a6ee878fa012db06c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-17 00:28:23 +0200

    feat: add useProducts custom hook with useQuery hook to build a query object and pass it to getProducts service

---
+++ src/hooks/useProducts.tsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProduts } from "../services/getProducts";
+import { useSearchParams } from "react-router-dom";
+import { Query } from "../types/interfaces";
+
+export const useProducts = () => {
+  const [searchParams] = useSearchParams();
+  const featured = searchParams.get("featured");
+  const name = searchParams.get("name");
+  const company = searchParams.get("company");
+  const query: Query = {};
+
+  if (featured) {
+    query.featured = featured;
+  }
+
+  if (name) {
+    query.name = name;
+  }
+
+  if (company) {
+    query.company = company;
+  }
+
+  const {
+    isLoading,
+    data: products,
+    isError,
+  } = useQuery({
+    queryKey: ["products", featured, name, company],
+    queryFn: () => getProduts(query),
+  });
+
+  return { isLoading, products, isError };
+};
