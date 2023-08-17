d444a22230788979be4fa6f4aa16e01a09573f72
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-17 12:26:12 +0200

    feat: new component Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" to="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function Footer() {
+  return (
+    <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+      <Typography variant="h6" align="center" gutterBottom>
+        Footer
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        align="center"
+        color="text.secondary"
+        component="p"
+      >
+        Something here to give the footer a purpose!
+      </Typography>
+      <Copyright />
+    </Box>
+  );
+}
