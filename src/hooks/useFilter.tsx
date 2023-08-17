b99de9bdac98bb15d86a3a98069f3455227e2778
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-17 17:18:43 +0200

    feat: useFilter custom hook to set url params based on filter

---
+++ src/hooks/useFilter.tsx
@@ -0,0 +1,19 @@
+import { useStore } from "../context/storeContext";
+
+export const useFilter = () => {
+  const { setSearch, setUrlParams } = useStore();
+  const setUrlFilter = (
+    setFilter: React.Dispatch<React.SetStateAction<string>>,
+    filterValue: string,
+    filterName: string
+  ) => {
+    setFilter(filterValue);
+    setSearch((search) => (!search ? true : search));
+    setUrlParams((params) => {
+      const newParams = { ...params, [filterName]: filterValue };
+      return newParams;
+    });
+  };
+
+  return setUrlFilter;
+};
