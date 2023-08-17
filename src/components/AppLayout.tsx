059a2404a72c025906d92fb02e77910cad9032f6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-17 09:39:11 +0200

    feat: use SearchBar inside AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,5 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import SearchBar from "./SearchBar";
+
+const defaultTheme = createTheme();
 
 export default function AppLayout() {
-  return <Outlet />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <SearchBar />
+      <Outlet />
+    </ThemeProvider>
+  );
 }
