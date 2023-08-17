7a8518029c6fdbe53de51cfdf149df72d75ac3d4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-17 12:27:46 +0200

    feat: use Header, Filters and Footer components in AppLayout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,6 +2,11 @@ import { Outlet } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import SearchBar from "./SearchBar";
+import Footer from "./Footer";
+import { Container } from "@mui/material";
+import Header from "./Header";
+import Box from "@mui/material/Box";
+import Filters from "./Filters";
 
 const defaultTheme = createTheme();
 
@@ -10,7 +15,22 @@ export default function AppLayout() {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <SearchBar />
-      <Outlet />
+      <main>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Header />
+            <Filters />
+          </Container>
+        </Box>
+        <Outlet />
+      </main>
+      <Footer />
     </ThemeProvider>
   );
 }
