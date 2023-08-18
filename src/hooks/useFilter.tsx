b58181d4fffcab201d213088cc419a90acad7fd9
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-18 09:36:08 +0200

    refactor: to use updateUrl functon from store context (useReducer)

--- src/hooks/useFilter.tsx
+++ src/hooks/useFilter.tsx
@@ -1,18 +1,12 @@
 import { useStore } from "../context/storeContext";
+import { Query } from "../types/interfaces";
 
 export const useFilter = () => {
-  const { setSearch, setUrlParams } = useStore();
-  const setUrlFilter = (
-    setFilter: React.Dispatch<React.SetStateAction<string>>,
-    filterValue: string,
-    filterName: string
-  ) => {
-    setFilter(filterValue);
-    setSearch((search) => (!search ? true : search));
-    setUrlParams((params) => {
-      const newParams = { ...params, [filterName]: filterValue };
-      return newParams;
-    });
+  const { updateUrl, search, urlParams } = useStore();
+  const setUrlFilter = (filterValue: string, filterName: string) => {
+    const newSearch = !search ? true : search;
+    const newUrlParams: Query = { ...urlParams, [filterName]: filterValue };
+    updateUrl(newSearch, filterName, filterValue, newUrlParams);
   };
 
   return { setUrlFilter };
