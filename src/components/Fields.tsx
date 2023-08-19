057179478e926443d4275baf803886382bd2b99d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-19 11:48:28 +0200

    feat: add logic to handle when a field checkbox is clicked in Fields component

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -4,23 +4,32 @@ import {
   FormControl,
   FormControlLabel,
   FormGroup,
-  FormHelperText,
   FormLabel,
 } from "@mui/material";
 import _ from "lodash";
-const handleChange = () => {};
+import { useStore } from "../context/storeContext";
+
 const allowedFields = ["name", "price", "featured", "rating", "company"];
 export default function Fields() {
+  const { fields, updateFields } = useStore();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+    const updatedFields = { ...fields, [name]: checked };
+    updateFields(updatedFields); // Update fields and URL
+  };
+
   return (
     <Box sx={{ display: "flex" }}>
       <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
         <FormLabel component="legend">Select Fields</FormLabel>
         <FormGroup sx={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
-          {allowedFields.map((field) => (
+          {allowedFields.map((field, index) => (
             <FormControlLabel
+              key={index}
               control={
                 <Checkbox
-                  checked={false}
+                  checked={fields[field] || false}
                   onChange={handleChange}
                   name={field}
                 />
@@ -29,7 +38,7 @@ export default function Fields() {
             />
           ))}
         </FormGroup>
-        <FormHelperText>Be careful</FormHelperText>
+        {/* <Button onClick={applyFilters}>Apply Filters</Button> */}
       </FormControl>
     </Box>
   );
