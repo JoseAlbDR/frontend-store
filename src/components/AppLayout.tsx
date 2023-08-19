118b6c94083071033a917f4e68a7499356f09426
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-20 00:28:58 +0200

    feat: add zIndex 100 to header

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -13,6 +13,7 @@ const Header = styled("div")(() => ({
   top: 0,
   boxShadow:
     "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
+  zIndex: 100,
 }));
 
 const defaultTheme = createTheme();
