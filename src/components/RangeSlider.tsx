e550f7e53ef0ee74c63997870c8c091016014b4d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-20 17:23:58 +0200

    feat: refactor RangeSlider component to use single slider and select list

--- src/components/RangeSlider.tsx
+++ src/components/RangeSlider.tsx
@@ -5,32 +5,53 @@ import Typography from "@mui/material/Typography";
 import { useProductsData } from "../hooks/useProductsData";
 import { CircularProgress } from "@mui/material";
 import { useStore } from "../context/storeContext";
+import SelectList from "./SelectList";
+import { useState } from "react";
+import _ from "lodash";
+import { IRangeSliderProps } from "../types/interfaces";
+export default function RangeSlider({ type }: IRangeSliderProps) {
+  const [sign, setSign] = useState("=");
 
-function valuetext(value: number) {
-  return `${value}°C`;
-}
-
-export default function RangeSlider() {
+  const signs = ["=", ">", ">=", "<", "<="];
   // const [value, setValue] = React.useState<number[]>([20, 37]);
   const { isLoading, maxPrice, minPrice } = useProductsData();
-  const { priceRange, updatePriceRange } = useStore();
+  const { updateNumericFilter } = useStore();
 
   if (isLoading) return <CircularProgress />;
   const handleChange = (_event: Event, newValue: number | number[]) => {
-    updatePriceRange(newValue as number[]);
+    const priceString = `${type}${sign}${newValue}`;
+    updateNumericFilter(priceString);
+  };
+
+  const handleSignChange = (event: string) => {
+    console.log(event);
+    setSign(event);
   };
 
   return (
-    <Box sx={{ width: 300 }}>
-      <Typography gutterBottom>Price</Typography>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "50%",
+        gap: "10px",
+      }}
+    >
+      <Typography gutterBottom>{_.capitalize(type)}</Typography>
+      <SelectList
+        data={signs}
+        value={sign}
+        onChange={handleSignChange}
+        label={"Operator"}
+      />
       <Slider
-        getAriaLabel={() => "Temperature range"}
-        value={priceRange}
-        onChange={handleChange}
-        valueLabelDisplay="auto"
-        getAriaValueText={valuetext}
+        defaultValue={minPrice}
+        aria-label="Default"
         max={maxPrice}
         min={minPrice}
+        onChange={handleChange}
+        valueLabelDisplay="on"
       />
     </Box>
   );
